3e29c1304633674896819f914e1d3f2fc74d2b05
Jacob A. Clark <77306976+Triv-Code@users.noreply.github.com>
2021-06-22 20:37:17 -0400

    herb Object

---
+++ ah.js
@@ -0,0 +1,34 @@
+const fetch = require('node-fetch');
+// fetch('https://us.api.blizzard.com/data/wow/search/connected-realm?namespace=dynamic-us&status.type=UP&realms.timezone=America%2FNew_York&orderby=id&_page=1&access_token=US5MdIaBbE5FedcA7R4d6HP2256fe2kFBQ')
+//     .then(response => response.json())
+//     .then(data => console.log(data))
+
+const herbObject = [
+    {
+        name: "Marrowroot", 
+        id: 168589,
+        img: "https://render-us.worldofwarcraft.com/icons/56/inv_misc_herb_marrowroot.jpg",
+    },{
+        name: "Rising Glory", 
+        id: 168586,
+        img: "https://render-us.worldofwarcraft.com/icons/56/inv_misc_herb_marrowroot.jpg",
+    },{
+        name: "Vigil's Torch", 
+        id: 170554,
+        img: "https://render-us.worldofwarcraft.com/icons/56/inv_misc_herb_ardenweald.jpg",
+    },{
+        name: "Widowbloom", 
+        id: 168583,
+        img: "https://render-us.worldofwarcraft.com/icons/56/inv_misc_herb_bloodcup.jpg",
+    },{
+        name: "Death Blossom", 
+        id: 169701,
+        img: "https://render-us.worldofwarcraft.com/icons/56/inv_misc_herb_deathblossom.jpg",
+    },{
+        name: "Nightshade", 
+        id: 171315,
+        img: "https://render-us.worldofwarcraft.com/icons/56/inv_misc_herb_nightshade.jpg",
+    },
+]
+
+
