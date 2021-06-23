1a34b16a87d4ad1713af4a56e52764e9067de8a2
Jacob A. Clark <77306976+Triv-Code@users.noreply.github.com>
2021-06-23 02:43:19 -0400

    Added: CSS & Potions

--- ah.js
+++ ah.js
@@ -1,9 +1,5 @@
 const fetch = require('node-fetch');
-// fetch('https://us.api.blizzard.com/data/wow/search/connected-realm?namespace=dynamic-us&status.type=UP&realms.timezone=America%2FNew_York&orderby=id&_page=1&access_token=US5MdIaBbE5FedcA7R4d6HP2256fe2kFBQ')
-//     .then(response => response.json())
-//     .then(data => console.log(data))
-
-const herbObject = [
+const herbs = [
     {
         name: "Marrowroot", 
         id: 168589,
@@ -11,7 +7,7 @@ const herbObject = [
     },{
         name: "Rising Glory", 
         id: 168586,
-        img: "https://render-us.worldofwarcraft.com/icons/56/inv_misc_herb_marrowroot.jpg",
+        img: "https://render-us.worldofwarcraft.com/icons/56/inv_misc_herb_risingglory.jpg",
     },{
         name: "Vigil's Torch", 
         id: 170554,
@@ -30,5 +26,59 @@ const herbObject = [
         img: "https://render-us.worldofwarcraft.com/icons/56/inv_misc_herb_nightshade.jpg",
     },
 ]
+const potions = [
+    {
+        name: "Flask of Power",
+        id: 171276,
+        img: "https://render-us.worldofwarcraft.com/icons/56/inv_alchemy_90_flask_green.jpg",
+    },{
+        name: "Potion of Phantom Fire",
+        id: 171349,
+        img: "https://render-us.worldofwarcraft.com/icons/56/inv_alchemy_90_combat1_green.jpg",
+    },{
+        name: "Potion of Empowered Exorcisms",
+        id: 307381,
+        img: "https://render-us.worldofwarcraft.com/icons/56/inv_alchemy_90_combat1_pink.jpg",
+    },{
+        name: "Potion of Deathly Fixation",
+        id: 171351,
+        img: "https://render-us.worldofwarcraft.com/icons/56/inv_alchemy_90_combat1_yellow.jpg",
+    },{
+        name: "Potion of Spectral Agility",
+        id: 171270,
+        img: "https://render-us.worldofwarcraft.com/icons/56/inv_alchemy_90_combat2_green.jpg",
+    },{
+        name: "Potion of Spectral Strength",
+        id: 171275,
+        img: "https://render-us.worldofwarcraft.com/icons/56/inv_alchemy_90_combat2_yellow.jpg",
+    },{
+        name: "Potion of Spectral Intellect",
+        id: 171273,
+        img: "https://render-us.worldofwarcraft.com/icons/56/inv_alchemy_90_combat2_purple.jpg",
+    },{
+        name: "Potion of Hidden Spirit",
+        id: 171266,
+        img: "https://render-us.worldofwarcraft.com/icons/56/inv_alchemy_90_utility_red.jpg",
+    },
+]
 
+fetch('https://us.api.blizzard.com/data/wow/connected-realm/61/auctions?namespace=dynamic-us&locale=en_US&access_token=US5MdIaBbE5FedcA7R4d6HP2256fe2kFBQ')
+    .then(response => response.json())
+    .then(data => console.log(data))
 
+// herbs.forEach(herb => console.log(herb.name + " " + herb.id));
+
+// herbs.forEach(herb => {
+//     const herbName = herb.name;
+//     const herbImg = '<img src='+ herb.img +'></img>';
+//     const d1 = document.getElementsByTagName('div');
+//     document.body.innerHTML = herbImg + herbName;
+//     console.log('huh?');
+// })
+
+// herbs.forEach(herb => {
+//     const herbName = herb.name;
+//     const herbImg = '<img src='+ herb.img +'></img>';
+//     const d1 = document.getElementById("here");
+//     d1.insertAdjacentHTML('afterbegin', herbImg + herbName);
+// })
