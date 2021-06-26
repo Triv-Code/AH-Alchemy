3e8b7e4cd8fe3e0bb703a23cb2b769280942811c
Jacob A. Clark <77306976+Triv-Code@users.noreply.github.com>
2021-06-26 04:51:27 -0400

    Added Herb List / Tab / JS / Adjusted CSS

--- ah.js
+++ ah.js
@@ -66,10 +66,9 @@ const potions = [
 //     .then(response => response.json())
 //     .then(data => console.log(data.auctions))
 
-// --- ID's for List Columns 1/2
-const list = document.getElementById("myList");
-const listTwo = document.getElementById("secList");
-// --- Arrays for Potion Name / Potion Image
+// --- Start of Potion Section
+const potionList_1 = document.getElementById("potionList_1");
+const potionList_2 = document.getElementById("potionList_2");
 const potName = [];
 const potImg = [];
 const potCost = []; // - Once Fetch is Parsing Cost
@@ -79,20 +78,47 @@ potions.forEach(potion => {
     potImg.push(potion.img);
 })
 // --- Places HTML for Lists --- Separates into 2 Columns for Styling. 
-for (let i = 0; i < 8; i++) {
+for (let i = 0; i < potName.length; i++) {
     let li = document.createElement('li');
     let img = document.createElement('img');
     let liGold = document.createElement('li');
     li.innerHTML = '<img src="' + potImg[i] + '" class="icon">' + '<p1 class="objName">' + potName[i] + '</p1>';
     liGold.innerHTML = '<li class="gold">Material Cost: 300<img src="img/gold.png" class="coinImg"> 25<img src="img/silver.png" class="coinImg"> | Buy Out: 354<img src="img/gold.png" class="coinImg"> 78<img src="img/silver.png" class="coinImg"></li>';
-    if ( i < 4) {
-        list.append(li);
-        list.append(liGold);
+    if ( i <  potName.length / 2) {
+        potionList_1.append(li);
+        potionList_1.append(liGold);
     } else {
-        listTwo.append(li);
-        listTwo.append(liGold);
+        potionList_2.append(li);
+        potionList_2.append(liGold);
     };
 };
+// --- Start of Herb Section
+const herbList_1 = document.getElementById("herbList_1");
+const herbList_2 = document.getElementById("herbList_2");
+const herbName = [];
+const herbImg = [];
+const herbCost = [];
+// --- Creates separate Arrays for Herb Name / Herb Image
+herbs.forEach(herb => {
+    herbName.push(herb.name);
+    herbImg.push(herb.img);
+})
+// --- Herb List Loop
+for (let i = 0; i < herbName.length; i++) {
+    let li = document.createElement('li');
+    let img = document.createElement('img');
+    let liGold = document.createElement('li');
+    li.innerHTML = '<img src="' + herbImg[i] + '" class="icon">' + '<p1 class="objName">' + herbName[i] + '</p1>';
+    liGold.innerHTML = '<li class="gold">Cost Per Herb: 300<img src="img/gold.png" class="coinImg"> 25<img src="img/silver.png" class="coinImg"></li>';
+        if ( i < herbName.length / 2) {
+            herbList_1.append(li);
+            herbList_1.append(liGold);
+        } else {
+            herbList_2.append(li);
+            herbList_2.append(liGold);
+        };
+}
+
 // --- Changes AH Buyout into Gold/Silver/Copper 
 let gold = '';
 let silver = '';
@@ -108,3 +134,18 @@ function currency(num) {
     return silver;
     return gold;
 };
+
+// --- Hide / Show divs
+function openPage(evt, pageName) {
+    let i, pageContent, link;
+    pageContent = document.getElementsByClassName("pageContent");
+    for (i = 0; i < pageContent.length; i++) {
+      pageContent[i].style.display = "none";
+    }
+    link = document.getElementsByClassName("link");
+    for (i = 0; i < link.length; i++) {
+      link[i].className = link[i].className.replace(" active", "");
+    }
+    document.getElementById(pageName).style.display = "flex";
+    evt.currentTarget.className += " active";
+  }
